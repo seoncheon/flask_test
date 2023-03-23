cf897ef96665446cfc816afd56fc0cecd57d756b
seoncheon <kimi9868@gmail.com>
2023-03-23 13:06:39 +0900

    feat:라우트 추가 및 추가 개선사항(blueprint) 확인

--- basic/f2.py
+++ basic/f2.py
@@ -1,1 +1,27 @@
-# 새로운 브랜치 basic에서 작업이 진행됨
+'''
+    라우트 추가 = URL 추가
+'''
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+
+app = Flask(__name__)
+
+# 기획서를 기반해서 총 페이지 수만큼 URL을 준비
+# 뼈대를 먼저 잡아서 각 페이지에 해당하는 URL 준비
+# blueprint를 사용한다면 대분류, 중분류, 소분류 등 트리구조로 배치
+# /login <-> blueprint 활용 : /auth/users/login
+
+@app.route('/')
+def home():
+    return "Hello world"
+
+# 아래와 같은 URL 구성은 blueprint를 사용하여 섹션을 나눠서 관리하는 것이 더 낫다
+@app.route('/auth/users/login')
+def login():
+    return "login page"
+
+@app.route('/auth/users/signup')
+def signup():
+    return "signup page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
